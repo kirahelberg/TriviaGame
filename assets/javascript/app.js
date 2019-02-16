bc35c48e201693e3b0eebc192d672f88317d723f
Kira Helberg <kira.helberg@gmail.com>
2019-02-15 22:32:13 -0500

    Setup Javascript Questions + Answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,71 @@
+//Create array for game questions + answers
+var gameQuestions = {
+  questions: [
+    {
+      q1:
+        "Question 1: What is the largest living organism visible from Earth's orbit?",
+      q2: "Question 2: What is the hottest planet in the solar system?",
+      q3: "Question 3: What planet has no moons?",
+      q4: "Question 4: What galaxy do we live in?",
+      q5: "Question 5: Who was the first man to walk on the moon?"
+    }
+  ],
+  choices: [
+    {
+      c1: [
+        "Great Barrier Reef off Australia",
+        "Rocky Mountains",
+        "Giant sequoias in California",
+        "Honey fungus colony in Oregon"
+      ],
+      c2: ["Jupiter", "Venus", "Mars", "Mercury"],
+      c3: ["Earth", "Saturn", "Mercury", "Uranus"],
+      c4: [
+        "Sombrero Galaxy",
+        "Whirlpool Galaxy",
+        "Andromeda Galaxy",
+        "Milky Way Galaxy"
+      ],
+      c5: ["Lance Armstrong", "Buzz Lightyear", "Neil Armstrong", "James Irwin"]
+    }
+  ],
+  answers: {
+    a1: "Great Barrier Reef off Australia",
+    a2: "Venus",
+    a3: "Mercury",
+    a4: "Milky Way Galaxy",
+    a5: "Neil Armstrong"
+  }
+};
+
+//Set up game variables
+let correctAnswers = 0;
+let incorrectAnswers = 0;
+let unansweredQuestions = 0;
+
+//Set up timer variables
+var clockRunning = false;
+var time = 0;
+document.getElementById("#countdownTimer").text("00:30");
+
+//Display the number of correct + incorrect answers and unanswered questions
+document.getElementById("correct-answers").text(correctAnswers);
+document.getElementById("incorrect-answers").text(incorrectAnswers);
+document.getElementById("unanswered-questions").text(unansweredQuestions);
+
+//Hide game questions if the game has not started
+$(document).ready(function() {
+  $("#game-container").hide();
+  $("#score-container").hide();
+});
+
+//Click button to start game
+function startGame() {
+  //When game starts, timer counts down from 30 sec + questions are revealed
+  $("#startButton").click(function() {
+    $("#game-container").show();
+  });
+}
+
+//Answers are counted as "correct", "incorrect", or "unanswered"
+//When the timer ends, game over and the results are displayed
