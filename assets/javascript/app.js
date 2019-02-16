96ed8b4e85b3962563e8bb6c7cedf667dfff1470
Kira Helberg <kira.helberg@gmail.com>
2019-02-16 15:17:30 -0500

    Added countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,56 +2,46 @@ var gameQuestions = [
   {
     question:
       "Question 1: What is the largest living organism visible from Earth's orbit?",
-    answers: {
-      A: "Great Barrier Reef",
-      B: "Rocky Mountains",
-      C: "Giant sequoias in California",
-      D: "Honey fungus colony in Oregon"
-    },
+    answers: [
+      "Great Barrier Reef",
+      "Rocky Mountains",
+      "Giant sequoias in California",
+      "Honey fungus colony in Oregon"
+    ],
     correctAnswer: "Great Barrier Reef off Australia"
   },
 
   {
     question: "Question 2: What is the hottest planet in the solar system?",
-    answers: {
-      A: "Jupiter",
-      B: "Venus",
-      C: "Mars",
-      D: "Mercury"
-    },
+    answers: ["Jupiter", "Venus", "Mars", "Mercury"],
     correctAnswer: "Venus"
   },
 
   {
     question: "Question 3: What planet has no moons?",
-    answers: {
-      A: "Earth",
-      B: "Saturn",
-      C: "Mercury",
-      D: "Uranus"
-    },
+    answers: ["Earth", "Saturn", "Mercury", "Uranus"],
     correctAnswer: "Mercury"
   },
 
   {
     question: "Question 4: What galaxy do we live in?",
-    answers: {
-      A: "Sombrero Galaxy",
-      B: "Whirlpool Galaxy",
-      C: "Andromeda Galaxy",
-      D: "Milky Way Galaxy"
-    },
+    answers: [
+      "Sombrero Galaxy",
+      "Whirlpool Galaxy",
+      "Andromeda Galaxy",
+      "Milky Way Galaxy"
+    ],
     correctAnswer: "Milky Way Galaxy"
   },
 
   {
     question: "Question 5: Who was the first man to walk on the moon?",
-    answers: {
-      A: "Lance Armstrong",
-      B: "Buzz Lightyear",
-      C: "Neil Armstrong",
-      D: "James Irwin"
-    },
+    answers: [
+      "Lance Armstrong",
+      "Buzz Lightyear",
+      "Neil Armstrong",
+      "James Irwin"
+    ],
     correctAnswer: "Neil Armstrong"
   }
 ];
@@ -60,11 +50,6 @@ var gameQuestions = [
 let correctAnswers = 0;
 let incorrectAnswers = 0;
 let unansweredQuestions = 0;
-
-//Set up timer variables
-var clockRunning = false;
-var time = 0;
-$("#countdownTimer").text("00:30");
 
 //Display the number of correct + incorrect answers and unanswered questions
 $("correct-answers").text(correctAnswers);
@@ -75,14 +60,13 @@ $("unanswered-questions").text(unansweredQuestions);
 $(document).ready(function() {
   $("#game-container").hide();
   $("#score-container").hide();
-  console.log("ready");
 
   //Click button to start game
   $("#startButton").on("click", function() {
     $("#start-container").hide();
-    console.log("start");
     $("#game-container").show();
     displayQuestions();
+    startTimer();
   });
 
   //Display game questions
@@ -93,19 +77,34 @@ $(document).ready(function() {
           gameQuestions[i].question +
           "</h4>" +
           "<input type='radio' id='answerChoices'>" +
-          gameQuestions[i].answers.A +
+          gameQuestions[i].answers[0] +
           "<input type='radio' id='answerChoices'>" +
-          gameQuestions[i].answers.B +
+          gameQuestions[i].answers[1] +
           "<input type='radio' id='answerChoices'>" +
-          gameQuestions[i].answers.C +
+          gameQuestions[i].answers[2] +
           "<input type='radio' id='answerChoices'>" +
-          gameQuestions[i].answers.D
+          gameQuestions[i].answers[3]
       );
     }
   }
 
   //When game starts, timer counts down from 30 sec
-  function startTimer() {}
+  function startTimer() {
+    var timeLeft = 30;
+    var intervalId = setInterval(countdown, 1000);
+    function countdown() {
+      if (timeLeft === 0) {
+        $("#game-container").hide();
+        $("#score-container").show();
+      } else if (timeLeft >= 10) {
+        timeLeft--;
+        $("#countdownTimer").text("00:" + timeLeft);
+      } else if (timeLeft <= 9) {
+        timeLeft--;
+        $("#countdownTimer").text("00:0" + timeLeft);
+      }
+    }
+  }
 
   //Answers are counted as "correct", "incorrect", or "unanswered"
   //When the timer ends, game over and the results are displayed
